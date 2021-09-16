afddc4cc5a1223fd4cd6d1b95f22dae0fc03215b
knightCh <amankumarpandey2397@gmail.com>
2021-09-16 09:23:22 +0530

    lesson-1 about selectors and filters

---
+++ Lesson1/main.js
@@ -0,0 +1,12 @@
+$(".text").css('border','2px solid orange');
+$("p:first").css('color','white');
+$("h3:last").css('color','red');
+$(".card:eq(2)").css('border','2px solid black');
+$(".card:even").css('background-color','#a52323');
+$('p:contains("Lorem")').css('color','orange');
+$('h3:not(:contains("Hello"))').css('font-size','25px');
+$("div[class='box']").css('background-color','blueviolet');
+$("[class!='box']").css('color','black');
+
+
+                     
