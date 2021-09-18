c6f89d6ac953d89376818c8be134d24c0a750527
knightCh <amankumarpandey2397@gmail.com>
2021-09-18 22:26:31 +0530

    we use  #mouseenter

--- Lesson3/main.js
+++ Lesson3/main.js
@@ -1,4 +1,7 @@
 $('.btn').on('click', function(e){
     alert("hello , Dost");
     e.stopPropagation();
+});
+$('.Intro__Text').on('mouseenter',function(){
+    alert("You just enter the mouse!");
 })
