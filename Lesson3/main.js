65a8dd0cd3a505200625c8cb6597837144fb6b92
knightCh <amankumarpandey2397@gmail.com>
2021-09-20 13:38:03 +0530

    adding ready funtion to use jquery before html

--- Lesson3/main.js
+++ Lesson3/main.js
@@ -1,9 +1,11 @@
-$('.btn').on('click', function(e){
-    alert("hello , Dost");
-    e.stopPropagation();
-});
-$('.Intro__Text').on('mouseenter',function(){
-    alert("You just enter the mouse!");
-    $(this).css('background','black');
-});
-$('.Intro__Text').off('mouseenter');
+$(function () {
+    $('.btn').on('click', function (e) {
+        alert("hello , Dost");
+        e.stopPropagation();
+    });
+    $('.Intro__Text').on('mouseenter', function () {
+        alert("You just enter the mouse!");
+        $(this).css('background', 'black');
+    });
+    $('.Intro__Text').off('mouseenter');
+}); // we add ready funtion to use javascript file above the html.
