d2ca795d73e88b0ca07b459c60bb9374bd4cd4a8
knightCh <amankumarpandey2397@gmail.com>
2021-09-20 20:17:28 +0530

    using shortcut #museenter

--- Lesson3/main.js
+++ Lesson3/main.js
@@ -12,22 +12,27 @@ $(function () {
     // $(window).scroll(function(){
     //     alert('hello , myself Aman')
     // })
-    $(window).resize(function(){
-        $('.s1').css('background-color','green');
+    $(window).resize(function () {
+        $('.s1').css('background-color', 'green');
     })
 
-    $('.btn').click(function(){
-        $('p').css('font-size','30px');
-        $('h2').css('font-size','26px');
-        $('h4').css('font-size','28px');
-        $('.Intro__Text').css('background-color','darkorange');
-        $('.Intro__Text').css('width','500px');
-        $('.Intro__Text').css('height','50vh');
+    $('.btn').click(function () {
+        $('p').css('font-size', '30px');
+        $('h2').css('font-size', '26px');
+        $('h4').css('font-size', '28px');
+        $('.Intro__Text').css('background-color', 'darkorange');
+        $('.Intro__Text').css('width', '500px');
+        $('.Intro__Text').css('height', '50vh');
     }); // using click function in short way
 
-    $('#ch__color').dblclick(function(){
-        $('.s1').css('background-color','#4b0082');
-        $('.s2').css('background-color','#add8e6');
-        $('.s3').css('background-color','#ffa07a');
+    $('#ch__color').dblclick(function () {
+        $('.s1').css('background-color', '#4b0082');
+        $('.s2').css('background-color', '#add8e6');
+        $('.s3').css('background-color', '#ffa07a');
+    })
+
+    $('p').mouseenter(function () {
+        $(this).css('background-color','black');
+        $(this).css('color','green');
     })
 }); // we add ready funtion to use javascript file above the html.
