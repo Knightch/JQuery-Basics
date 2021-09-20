d42a68ca4dee1a7bebd096f366331f843bd8e01b
knightCh <amankumarpandey2397@gmail.com>
2021-09-20 14:20:40 +0530

    using short cut of click

--- Lesson3/main.js
+++ Lesson3/main.js
@@ -1,13 +1,13 @@
 $(function () {
-    $('.btn').on('click', function (e) {
-        alert("hello , Dost");
-        e.stopPropagation();
-    });
-    $('.Intro__Text').on('mouseenter', function () {
-        alert("You just enter the mouse!");
-        $(this).css('background', 'black');
-    });
-    $('.Intro__Text').off('mouseenter');
+    // $('.btn').on('click', function (e) {
+    //     alert("hello , Dost");
+    //     e.stopPropagation();
+    // });
+    // $('.Intro__Text').on('mouseenter', function () {
+    //     alert("You just enter the mouse!");
+    //     $(this).css('background', 'black');
+    // });
+    // //$('.Intro__Text').off('mouseenter');
 
     $(window).scroll(function(){
         alert('hello , myself Aman')
@@ -15,4 +15,13 @@ $(function () {
     $(window).resize(function(){
         $('.s1').css('background-color','green');
     })
+
+    $('.btn').click(function(){
+        $('p').css('font-size','30px');
+        $('h2').css('font-size','26px');
+        $('h4').css('font-size','28px');
+        $('.Intro__Text').css('background-color','darkorange');
+        $('.Intro__Text').css('width','500px');
+        $('.Intro__Text').css('height','50vh');
+    }); // using click function in short way
 }); // we add ready funtion to use javascript file above the html.
