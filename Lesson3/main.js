98a3542df80a37226ae3ef9a0fe3dedc81d117c9
knightCh <amankumarpandey2397@gmail.com>
2021-09-20 13:56:15 +0530

    using scroll and resize funtion

--- Lesson3/main.js
+++ Lesson3/main.js
@@ -8,4 +8,11 @@ $(function () {
         $(this).css('background', 'black');
     });
     $('.Intro__Text').off('mouseenter');
+
+    $(window).scroll(function(){
+        alert('hello , myself Aman')
+    })
+    $(window).resize(function(){
+        $('.s1').css('background-color','green');
+    })
 }); // we add ready funtion to use javascript file above the html.
